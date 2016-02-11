bb0be9a16470a663ba72a417dc5c22a740291ca6
Rama Chandra <sriramsrinivasan@srirams-macbook-pro.local>
2016-02-11 09:52:05 +0000

    file update

--- tvOSVideoApp/PlayerViewController.swift
+++ tvOSVideoApp/PlayerViewController.swift
@@ -33,14 +33,6 @@ class PlayerViewController: AVPlayerViewController,AVPlayerViewControllerDelegat
     }
     
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
+    
 
 }
